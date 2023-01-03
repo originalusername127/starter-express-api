7ac5b4d76db365bd78ec462b3497cf52cb61ad46
originalusername127 <117903990+originalusername127@users.noreply.github.com>
2023-01-03 20:24:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const discord = require('discord.js')
+const dotenv = require('dotenv')
 const app = express()
+const client = new discord.Client({
+    intents: [discord.GatewayIntentBits.Guilds, discord.GatewayIntentBits.GuildMessages]
+})
+dotenv.config()
 
 app.get('/', (req, res) => {
     res.send("GO AWAY")
 })
 app.listen(3000)
+client.login(process.env.Token)
