ff4a27a22f440a4426426bb48a6a95d91df7079d
originalusername127 <117903990+originalusername127@users.noreply.github.com>
2023-01-03 19:18:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+app.get('/', (req, res) => {
+    res.send("GO AWAY")
 })
-app.listen(process.env.PORT || 3000)
+app.listen(3000)
